5953415fa79178e836e43194204079e30785864c
Michael Bestas <mkbestas@lineageos.org>
2024-06-24 00:32:53 +0300

    platina: Add symlinks
    Change-Id: I6eed2ca2574dd2bbb60a72337f525c555628d88b

--- platina-vendor.mk
+++ platina-vendor.mk
@@ -1123,3 +1123,13 @@ PRODUCT_PACKAGES += \
     SoterService \
     TimeService \
     vendor.qti.gnss@3.0-service
+
+PRODUCT_PACKAGES += \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
